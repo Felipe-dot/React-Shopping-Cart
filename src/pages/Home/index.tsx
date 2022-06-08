9f41a4af9db7421ab20f2562b0cde62d60a2cc38
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-06-07 21:28:06 -0300

    Developing the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -22,44 +22,60 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    return {};     
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+        const productData = (await api.get('products')).data;
+        var productFormat : ProductFormatted[] = [];
+
+        productData.forEach((element : Product) => {
+          productFormat.concat({
+            id: element.id,
+            title: element.title,
+            price: element.price,
+            image: element.image,
+            priceFormatted: formatPrice(element.price),
+          })
+        })
+
+        setProducts([...productFormat]);
     }
 
     loadProducts();
   }, []);
 
   function handleAddProduct(id: number) {
-    // TODO
+    addProduct(id);
   }
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
+      {products.map(product => (
+        <li>
+        <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
+        <strong>{product.title}</strong>
+        <span>{product.priceFormatted}</span>
         <button
           type="button"
           data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
+          onClick={() => handleAddProduct(product.id)}
         >
           <div data-testid="cart-product-quantity">
             <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
+            {cartItemsAmount[product.id] || 0} 2
           </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+      ))}
+     
     </ProductList>
   );
 };
