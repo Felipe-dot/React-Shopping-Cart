8967dac75f6d408a586b1ba9587fd533875f68a3
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-06-08 21:34:28 -0300

    Fixing cart size

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,7 @@ import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
-  const cartSize = cart.length - 1;
+  const cartSize = cart.length;
 
   return (
     <Container>
